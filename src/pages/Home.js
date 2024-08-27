22d71731497c6393a5bd54c8178a942259b07904
Divyanshi Varde <divyanshi.varde@propelius.tech>
2024-08-27 14:47:02 +0530

    Minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,7 +47,6 @@ const Home = () => {
             </button>
             <button onClick={() => setOrderBy("title")}>Title</button>
             <button onClick={() => setOrderBy("rating")}>Rating</button>
-            {orderBy}
           </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
